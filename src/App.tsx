abcb57a0d9f658d3bab817d62d34997601895688
KrunalMrathod <krunalrathod564@gmail.com>
2024-06-22 15:42:53 +0530

    Commit message

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
-export const App = () =>{
-    return <h1>react typescript webpack</h1>
-}
+import "./styles.css";
+import Image from "./IMAGE.png";
+import image from "./image.svg";
+import { Counter } from "./CliclCounter";
+export const App = () => {
+  const name = "KRUNAL";
+  return (
+    <>
+      <Counter />
+      <h1>
+        react typescript webpack - {process.env.NODE_ENV} {process.env.name}{" "}
+      </h1>
+      <img src={Image} alt="" width={"50%"} />
+      <img src={image} alt="" />
+    </>
+  );
+};
---
+++ src/CliclCounter.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+export const Counter = () => {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <h3>
+        {' '}
+        yohoo yohooo Edited Update the count and edit src/App.tsx, state is
+        preserved
+      </h3>
+      <button onClick={() => setCount((c) => c + 1)}>Count - {count}</button>
+    </div>
+  )
+}
