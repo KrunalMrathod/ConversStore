4a9b124a7068cae7f36165be5cc9cfb4079d7cb4
KrunalMrathod <krunalrathod564@gmail.com>
2024-07-07 10:37:55 +0530

    Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,52 @@
+import './Navbar.css'
+import NavLog from '../assest/Navlogo.png'
+import { FaRegUser, FaRegHeart } from 'react-icons/fa'
+import { FiShoppingCart } from 'react-icons/fi'
+import { IoSearch } from 'react-icons/io5'
 
-// import "./Navbar.css"
-import NavLog from "../assest/Navlogo.png";
-import { FaRegUser } from "react-icons/fa";
-import { FaRegHeart } from "react-icons/fa";
-import { FiShoppingCart } from "react-icons/fi";
-import { IoSearch } from "react-icons/io5";
 const Navbar = () => {
   return (
-    <div className="border-red-900 flex">
-      <div className="NavLeft">
-        <div className="NavLogo">
-          <img src= {NavLog} alt="" />
+    <div className="flex justify-between items-center shadow-custom-navbar h-16">
+      <div className="flex items-center w-2/5   h-full ">
+        <div className="w-[25%]  cursor-pointer h-full ">
+          <img src={NavLog} alt="Logo" className="w-full h-full object-cover" />
         </div>
-        <div className="NavLeftTabs">
-          <div className="TabLeft"> <span>News & Featured</span> </div>
-          <div className="TabLeft"> <span>Women</span> </div>
-          <div className="TabLeft"> <span>Men</span> </div>
-          <div className="TabLeft"> <span>Kids</span> </div>
-          <div className="TabLeft"> <span>Sale</span> </div>
+        <div className="flex justify-evenly  w-[70%] h-full items-center space-x-4 ">
+          <span className="cursor-pointer hover:opacity-70 text-slate-900 text-[15px] font-bold">
+            News & Featured
+          </span>
+          <span className="cursor-pointer hover:opacity-70 text-slate-900 text-[15px] font-bold">
+            Women
+          </span>
+          <span className="cursor-pointer hover:opacity-70 text-slate-900 text-[15px] font-bold">
+            Men
+          </span>
+          <span className="cursor-pointer hover:opacity-70 text-slate-900 text-[15px] font-bold">
+            Kids
+          </span>
+          <span className="cursor-pointer hover:opacity-70 text-slate-900 text-[15px] font-bold">
+            Sale
+          </span>
         </div>
       </div>
-      <div className="NavRight">
-        <div className="TabRight"> <span>SignIn</span> <FaRegUser/> </div>
-        <div className="TabRight"> <FaRegHeart/> </div>
-        <div className="TabRight"> <FiShoppingCart/> </div>
-        <div className="NavSearch">
-          <input type="serch" placeholder='Search' />
-          <IoSearch/>
+      <div className="flex justify-evenly space-x-4 w-1/3 justify-end  h-full">
+        <div className="flex items-center space-x-2 cursor-pointer">
+          <span className="text-slate-900 text-[15px] font-bold">Sign In</span>
+          <FaRegUser className="w-8 h-6" />
+        </div>
+        <div className="flex items-center cursor-pointer">
+          <FaRegHeart className="w-8 h-6" />
+        </div>
+        <div className="flex items-center cursor-pointer">
+          <FiShoppingCart className="w-8 h-6" />
+        </div>
+        <div className="flex items-center space-x-2 border-2 bg-black">
+          <input
+            type="search"
+            placeholder="Search"
+            className="border  p-1 rounded bg-transparent border-none outline-none text-white"
+          />
+          <IoSearch className="fill-slate-50 cursor-pointer h-full w-6" />
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'custom-navbar': 'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px',
+      }
+    }
   },
   plugins: [],
 };
